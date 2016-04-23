e3e1fc1de92e2c66bb987b1b955d8a8cf4d8f4a0
Nicolas Norswap Laurent <norswap@gmail.com>
2016-04-23 16:58:46 +0200

    add `expr` utility function

--- src/norswap/violin/Utils.kt
+++ src/norswap/violin/Utils.kt
@@ -2,6 +2,12 @@
 package norswap.violin.utils
 
 /**
+ * Returns the result of [f].
+ * The point of to allow statements in an expression context.
+ */
+inline fun <T> expr(f: () -> T): T = f()
+
+/**
  * Returns the receiver after evaluating [f] on it.
  */
 inline infix fun <T> T.after(f: (T) -> Unit): T {
